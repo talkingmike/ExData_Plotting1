59fa558dad1b5a78471d81c4a5649f0bfdf7ad75
Michael Foy <michaelfoy01@hotmail.com>
2014-05-11 00:57:40 -0500

    Plot1 files (R script, PNG file)

---
+++ plot1.R
@@ -0,0 +1,60 @@
+loadData <- function() {
+     ## 1) Load and format the data set, downloading the source if necessary
+     
+     ## Source data file is assumed to be ./data/ehpc.txt. If it does not 
+     ## exist, download and/or extract it
+     if (!file.exists("./data/ehpc.txt")) {
+          
+          ## If the zip file does not exist, download it
+          if (!file.exists("./data/ehpc.zip")) {
+               if (!file.exists("./data")) {
+                    dir.create("./data")
+               }
+               
+               fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+               download.file(fileURL,"./data/ehpc.zip")
+          }
+          
+          ## Extract data source
+          unzip("./data/ehpc.zip", exdir="./data")
+     }
+     sourceFilename <- "./data/household_power_consumption.txt"
+     
+     
+     ## Load data into data frame, subset to correct rows, clean up data
+     data <- suppressWarnings(fread(sourceFilename, sep=";", header=TRUE))
+     data <- data[(data$Date == "1/2/2007") | (data$Date == "2/2/2007"),]
+     
+     
+     
+     ## This isn't working, commenting out
+     ## Paste Date and Time columns together into one column
+     # tempDT <- paste(data$Date, data$Time, sep = " ")
+     # data$DateTime <- tempDT
+     
+     ## Convert Date and Time column from character to POSIXlt class
+     # data$DateTime <- strptime(data$DateTime, "%d/%m/%Y %H:%M:%S")
+     
+     ## Convert Date column from character to Date class
+     data$Date <- as.Date(data$Date, format="%d/%m/%y")
+     
+     ## Convert measurement columns from character to numeric class
+     data$Global_active_power <- as.numeric(data$Global_active_power)
+     data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+     data$Voltage <- as.numeric(data$Voltage)
+     data$Global_intensity <- as.numeric(data$Global_intensity)
+     data$Sub_metering_1 <- as.numeric(data$Sub_metering_1)
+     data$Sub_metering_2 <- as.numeric(data$Sub_metering_2)
+     data$Sub_metering_3 <- as.numeric(data$Sub_metering_3)
+     
+     return(data)
+}
+
+plot1 <- function() {
+     gap <- loadData()
+     
+     png(file="./plot1.png")
+     hist(gap$Global_active_power, main="Global Active Power", 
+          xlab="Global Active Power (kilowatts)", col="red")
+     dev.off()
+}
